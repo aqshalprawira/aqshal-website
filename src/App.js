7d8637b01d8bbd4b634dacd33842e5c60042214a
aqshalprawira <aqshal.map@gmail.com>
2023-06-15 01:28:21 +0700

    add logo navbar home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { VStack, Flex, Heading, Spacer } from "@chakra-ui/layout";
 import { IconButton } from "@chakra-ui/button";
 import { useColorMode } from "@chakra-ui/color-mode";
-import {FaSun, FaMoon, FaInstagram, FaGithub, FaLinkedin} from "react-icons/fa"
+import {FaSun, FaMoon, AiOutlineCode, FaGithub, FaLinkedin} from "react-icons/fa"
 import React, { useEffect } from "react";
+import { Image, Text, Box } from '@chakra-ui/react'
+import AqshalLight from "./assets/Aqshal-light.png"
+import AqshalDark from "./assets/Aqshal-dark.png"
 import Social from "./components/Social";
 import Profile from "./components/Profile";
 import Header from "./components/Header";
@@ -21,14 +24,16 @@ function App() {
     
     <VStack p={5}>
       <Flex w="100%">
-        <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">
-          Aqshal Prawira
-        </Heading>
+        { isDark ? (
+          <Image src={AqshalDark} w="170px" h="80px" cursor="pointer" onClick={(e) => {e.preventDefault(); window.location.href='https://aqshaldev.my.id/';}}/> 
+        ) : (
+          <Image src={AqshalLight} w="170px" h="80px" cursor="pointer" onClick={(e) => {e.preventDefault(); window.location.href='https://aqshaldev.my.id/';}} /> 
+        )
+          }
         <Spacer></Spacer>
-        <IconButton ml={2} icon={<FaInstagram/>} isRound="true" onClick={(e) => {e.preventDefault(); window.location.href='https://www.instagram.com/aqshalprwira/';}}></IconButton>
         <IconButton ml={2} icon={<FaGithub/>} isRound="true" onClick={(e) => {e.preventDefault(); window.location.href='https://github.com/aqshalprawira';}}></IconButton>
         <IconButton ml={2} icon={<FaLinkedin/>} isRound="true" onClick={(e) => {e.preventDefault(); window.location.href='https://www.linkedin.com/in/aqshalprawira/';}}></IconButton>
-        <IconButton ml={8} icon={isDark ? <FaSun /> : <FaMoon />} isRound="true" onClick={toggleColorMode}></IconButton>
+        <IconButton ml={10} icon={isDark ? <FaMoon /> : <FaSun />} isRound="true" onClick={toggleColorMode}></IconButton>
       </Flex>
       <div>
           Ntar malem dilanjutin, bobo dulu 	&#128564;
