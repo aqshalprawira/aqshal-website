f89ce2037cd084afc8e6c70cc02de6722def7390
aqshalprawira <aqshal.map@gmail.com>
2023-06-28 09:34:13 +0700

    add skills

--- src/App.js
+++ src/App.js
@@ -25,15 +25,21 @@ import {
   Card,
   CardBody,
   Stack,
+  Wrap,
+  WrapItem,
 } from "@chakra-ui/react";
 import ProfilePicture from "./assets/aqshal-pp.jpg";
 import Project1 from "./assets/project-1.png";
 import Project2 from "./assets/project-2.png";
 import Project3 from "./assets/project-3.png";
-import Typed from 'react-typed';
-import Fade from 'react-reveal/Fade';
-
-
+import Typed from "react-typed";
+import Fade from "react-reveal/Fade";
+import reactLogo from "../src/assets/reactLogo.png";
+import cssLogo from "../src/assets/cssLogo.png";
+import gitLogo from "../src/assets/gitLogo.png";
+import htmlLogo from "../src/assets/htmlLogo.png";
+import jsLogo from "../src/assets/jsLogo.png";
+import tailwindLogo from "../src/assets/tailwindLogo.svg";
 
 function App() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -87,7 +93,7 @@ function App() {
         >
           About Me
         </Button>
-          
+
         <Button
           _hover={{
             transform: "scale(1.2)",
@@ -105,9 +111,27 @@ function App() {
         >
           My Projects
         </Button>
+
+        <Button
+          _hover={{
+            transform: "scale(1.2)",
+            opacity: 1,
+            color: isDark ? "cyan" : "darkblue",
+          }}
+          mt={1}
+          ml={2}
+          opacity={0.5}
+          size={["xs", "sm"]}
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = "#skills";
+          }}
+        >
+          Skills
+        </Button>
         <Spacer />
         <IconButton
-          ml={5}
+          ml={4}
           icon={isDark ? <FaSun /> : <FaMoon />}
           isRound="true"
           onClick={toggleColorMode}
@@ -154,49 +178,46 @@ function App() {
             Hello There &#x1F44B;
           </Text>
           <Box as="p" fontSize={["md", "2xl"]} fontWeight="bold">
-          <Typed 
-            strings={[
-              "My Name is Muhammad Aqshfj",
-              "My Name is Muhammad Aqshal Prawira",
-            ]}
-            typeSpeed={100}
-            backSpeed={100}
-            loop 
-          />
+            <Typed
+              strings={[
+                "My Name is Muhammad Aqshfj",
+                "My Name is Muhammad Aqshal Prawira",
+              ]}
+              typeSpeed={100}
+              backSpeed={100}
+            />
           </Box>
           <Text fontSize={["sm", "md"]} mt={["5px", "10px"]}>
             I am a 7th-semester student at{" "}
-            <Text as="strong">
+            <Text as="strong" color={isDark ? "lightgreen" : "green"}>
               {" "}
               UPN "VETERAN" JAKARTA,
             </Text>{" "}
             majoring in Information Systems. I have a strong interest in{" "}
-            <Text as="strong">
-              front-end web development.
-            </Text>{" "}
-            Currently, I have been studying various programming languages
-            relevant to front-end web development, such as{" "}
-            <Text as="strong">
+            <Text as="strong">front-end web development.</Text> Currently, I
+            have been studying various programming languages relevant to
+            front-end web development, such as{" "}
+            <Text as="strong" color="orange">
               HTML,{" "}
             </Text>
             popular frameworks like{" "}
-            <Text as="strong" >
+            <Text as="strong" color={isDark ? "lightblue" : "darkblue"}>
               Tailwind CSS
             </Text>{" "}
             and{" "}
-            <Text as="strong">
+            <Text as="strong" color={isDark ? "lightblue" : "darkblue"}>
               Chakra UI.
             </Text>{" "}
             Additionally, I have experience in using{" "}
-            <Text as="strong">
+            <Text as="strong" color={isDark ? "yellow" : "orange"}>
               JavaScript
             </Text>{" "}
             frameworks such as{" "}
-            <Text as="strong">
+            <Text as="strong" color={isDark ? "lightblue" : "darkblue"}>
               React JS.
-            </Text>{" "}
-            I am highly enthusiastic about further developing my skills in this
-            field and seeking more experiences in the IT industry.
+            </Text>
+            {"  "}I am highly enthusiastic about further developing my skills in
+            this field and seeking more experiences in the IT industry.
           </Text>
           <IconButton
             mt={4}
@@ -221,7 +242,10 @@ function App() {
             isRound="true"
             onClick={(e) => {
               e.preventDefault();
-              window.open("https://www.linkedin.com/in/aqshalprawira/","_blank");
+              window.open(
+                "https://www.linkedin.com/in/aqshalprawira/",
+                "_blank"
+              );
             }}
             _hover={{
               transform: "scale(1.5)",
@@ -233,8 +257,7 @@ function App() {
           ></IconButton>
         </Box>
       </Flex>
-      
-            
+
       <Flex
         w="100%"
         mt={5}
@@ -247,86 +270,171 @@ function App() {
         </Text>
       </Flex>
 
-      <Fade top delay={1500}>
-      <Flex
-        w="100%"
-        mt={5}
-        direction={["column", "row"]}
-        justifyContent={["center", "space-evenly"]}
-        alignItems={["center", "flex-start"]}
-        id="myprojects"
-        gap={6}
-      >
-        <Card maxW="sm" mt={5}>
-          <CardBody>
-            <Image src={Project1} alt="Dummy Image" borderRadius="lg" />
-            <Stack mt="6" spacing="3">
-              <Heading size="md">RuangPerpus</Heading>
-              <Text>
-                RuangPerpus is a library website that utilizes the Create, Read,
-                Update, and Delete (CRUD) functions to manage books
-              </Text>
-            </Stack>
-            <Box mt={2}>
-              <IconButton icon={<FaHtml5 />} isRound="true"></IconButton>
-              <IconButton
-                icon={<SiTailwindcss />}
-                isRound="true"
-                ml={2}
-              ></IconButton>
-              <IconButton
-                icon={<SiJavascript />}
-                isRound="true"
-                ml={2}
-              ></IconButton>
-            </Box>
-          </CardBody>
-        </Card>
+      <Fade top>
+        <Flex
+          w="100%"
+          mt={5}
+          direction={["column", "row"]}
+          justifyContent={["center", "space-evenly"]}
+          alignItems={["center", "flex-start"]}
+          id="myprojects"
+          gap={6}
+        >
+          <Card maxW="sm" mt={5}>
+            <CardBody>
+              <Image src={Project1} alt="Dummy Image" borderRadius="lg" />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">RuangPerpus</Heading>
+                <Text>
+                  RuangPerpus is a library website that utilizes the Create,
+                  Read, Update, and Delete (CRUD) functions to manage books
+                </Text>
+              </Stack>
+              <Box mt={2}>
+                <IconButton icon={<FaHtml5 />} isRound="true"></IconButton>
+                <IconButton
+                  icon={<SiTailwindcss />}
+                  isRound="true"
+                  ml={2}
+                ></IconButton>
+                <IconButton
+                  icon={<SiJavascript />}
+                  isRound="true"
+                  ml={2}
+                ></IconButton>
+              </Box>
+            </CardBody>
+          </Card>
 
-        <Card maxW="sm" mt={5}>
-          <CardBody>
-            <Image src={Project2} alt="Student Portal" borderRadius="lg" />
-            <Stack mt="6" spacing="3">
-              <Heading size="md">Student Portal</Heading>
-              <Text>
-                Student Portal is a website with Create, Read, Update, and
-                Delete (CRUD) functionality specifically designed to manage and
-                organize student data.
-              </Text>
-            </Stack>
-            <Box mt={2}>
-              <IconButton icon={<FaHtml5 />} isRound="true"></IconButton>
-              <IconButton
-                icon={<SiChakraui />}
-                isRound="true"
-                ml={2}
-              ></IconButton>
-              <IconButton
-                icon={<GrReactjs />}
-                isRound="true"
-                ml={2}
-              ></IconButton>
-            </Box>
-          </CardBody>
-        </Card>
+          <Card maxW="sm" mt={5}>
+            <CardBody>
+              <Image src={Project2} alt="Student Portal" borderRadius="lg" />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">Student Portal</Heading>
+                <Text>
+                  Student Portal is a website with Create, Read, Update, and
+                  Delete (CRUD) functionality specifically designed to manage
+                  and organize student data.
+                </Text>
+              </Stack>
+              <Box mt={2}>
+                <IconButton icon={<FaHtml5 />} isRound="true"></IconButton>
+                <IconButton
+                  icon={<SiChakraui />}
+                  isRound="true"
+                  ml={2}
+                ></IconButton>
+                <IconButton
+                  icon={<GrReactjs />}
+                  isRound="true"
+                  ml={2}
+                ></IconButton>
+              </Box>
+            </CardBody>
+          </Card>
 
-        <Card maxW="sm" mt={5}>
-          <CardBody>
-            <Image src={Project3} alt="Personal Website" borderRadius="lg" />
-            <Stack mt="6" spacing="3">
-              <Heading size="md">MeMasak</Heading>
-              <Text>
-                MeMasak is a website that provides a wide range of food recipes
-                for users to learn and explore their culinary skills.
-              </Text>
-            </Stack>
-            <Box mt={2}>
-              <IconButton icon={<FaHtml5 />} isRound="true"></IconButton>
-              <IconButton icon={<SiCss3 />} isRound="true" ml={2}></IconButton>
-            </Box>
-          </CardBody>
-        </Card>
-      </Flex>
+          <Card maxW="sm" mt={5}>
+            <CardBody>
+              <Image src={Project3} alt="Personal Website" borderRadius="lg" />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">MeMasak</Heading>
+                <Text>
+                  MeMasak is a website that provides a wide range of food
+                  recipes for users to learn and explore their culinary skills.
+                </Text>
+              </Stack>
+              <Box mt={2}>
+                <IconButton icon={<FaHtml5 />} isRound="true"></IconButton>
+                <IconButton
+                  icon={<SiCss3 />}
+                  isRound="true"
+                  ml={2}
+                ></IconButton>
+              </Box>
+            </CardBody>
+          </Card>
+        </Flex>
+      </Fade>
+
+      <Fade top>
+        <Flex
+          w="100%"
+          mt={5}
+          direction={["column", "column"]}
+          justifyContent={["center", "center"]}
+          alignItems={["center", "center"]}
+          gap={6}
+          id="skills"
+        >
+          <Text fontSize={["xl", "4xl"]} fontWeight="bold">
+            Skills
+          </Text>
+          <Wrap w={["50%", "100%"]}>
+            <WrapItem
+              _hover={{
+                transform: "scale(1.2)",
+                opacity: 1,
+              }}
+              transition="transform 0.3s"
+              opacity={0.5}
+            >
+              <Image src={reactLogo} />{" "}
+            </WrapItem>
+            <WrapItem
+              _hover={{
+                transform: "scale(1.2)",
+                opacity: 1,
+              }}
+              transition="transform 0.3s"
+              opacity={0.5}
+            >
+              <Image src={cssLogo} />{" "}
+            </WrapItem>
+            <WrapItem
+              _hover={{
+                transform: "scale(1.2)",
+                opacity: 1,
+              }}
+              transition="transform 0.3s"
+              opacity={0.5}
+            >
+              {" "}
+              <Image src={htmlLogo} />{" "}
+            </WrapItem>
+            <WrapItem
+              _hover={{
+                transform: "scale(1.2)",
+                opacity: 1,
+              }}
+              transition="transform 0.3s"
+              opacity={0.5}
+            >
+              {" "}
+              <Image src={jsLogo} />{" "}
+            </WrapItem>
+            <WrapItem
+              _hover={{
+                transform: "scale(1.2)",
+                opacity: 1,
+              }}
+              transition="transform 0.3s"
+              opacity={0.5}
+            >
+              <Image src={tailwindLogo} boxSize="48px" />{" "}
+            </WrapItem>
+            <WrapItem
+              _hover={{
+                transform: "scale(1.2)",
+                opacity: 1,
+              }}
+              transition="transform 0.3s"
+              opacity={0.5}
+            >
+              {" "}
+              <Image src={gitLogo} />{" "}
+            </WrapItem>
+          </Wrap>
+        </Flex>
       </Fade>
     </VStack>
   );
